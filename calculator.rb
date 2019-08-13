8fc643539c9916b896753e6c3dad76109e41e3ac
Richard Huelves <rhuelves@gmail.com>
2019-08-13 20:43:38 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number =
+ expect(first_number).to be_an(Integer).or be_a(Float)
+
+second_number = 
+ expect(second_number).to be_an(Integer).or be_a(Float)
+ expect(second_number).not_to equal(0)
