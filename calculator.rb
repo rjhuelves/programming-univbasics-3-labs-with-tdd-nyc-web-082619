1509b5a1c24c767948d75e9ac620a4dbab8f1816
Richard Huelves <rhuelves@gmail.com>
2019-08-13 21:23:57 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,17 +1,8 @@
-first_number = ""
- expect(first_number).to be_an(Integer).or be_a(Float)
-
-second_number = ""
- expect(second_number).to be_an(Integer).or be_a(Float)
- expect(second_number).not_to equal(0)
- 
- sum = ""
-  expect(sum).to eq(first_number+second_number)
-  
-difference = ""
-  expect(difference).to eq(first_number-second_number)
-
-product = ""
+first_number = 1
+second_number = 2
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
   expect(product).to eq(first_number*second_number)
 
 quotient = " "
